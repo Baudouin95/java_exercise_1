47c13f06aa3b48c20a175b20e8d8acfa867a2317
Baudouin95 <tekoubo@gmail.com>
2021-11-30 08:54:24 +0100

    une première commande

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,21 +1,61 @@
+
 import java.util.Scanner;
 public class Launcher {
+
+    public static int fibo(int n){
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+        else
+            return fibo(n - 1) + fibo(n - 2);
+    }
 
     public static void main(String[] args) {
         System.out.println("Bienvenue !!!");
 
         Scanner sc = new Scanner(System.in);
+        int resultat = 0;
         boolean a = true;
         while(a)
         {
+            boolean b =true;
             System.out.println("Veuillez saisir un mot : ");
             String str = sc.nextLine();
-            if(str.equals("quit")){
-                break;
+            switch(str){
+
+                case "quit":
+                    b = false;
+                    break;
+
+                case "fibo":
+                    int nbre = 0;
+                    boolean c = true;
+                    while (c){
+                        System.out.println("Donnez la valeur de n : ");
+                        str = sc.nextLine();
+                        str = str.trim();
+                        try{
+                            nbre = Integer.parseInt(str);
+                            c = false;
+                        }
+                        catch(NumberFormatException nfe){
+
+                        }
+                    }
+                    resultat = fibo(nbre);
+                    System.out.println("le " + nbre + "ieme element de la suite est : " + resultat);
+
+                    b = false;
+                    break;
+
+                default:
+                    break;
             }
-            else {
-                System.out.println("Unknown command");
-                System.out.println("Veuillez saisir un mot : ");
+
+
+            if(!b){
+                break;
             }
         }
     }
